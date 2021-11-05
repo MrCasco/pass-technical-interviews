2ce49d603d3eb2a4270033399b195aa1aafbb899
MrCasco <ernesto.casco.velazquez@hotmail.com>
2021-11-04 18:49:35 -0600

    Changes linked_list_bubble_sort

--- bubble_sort_linked_list.py
+++ bubble_sort_linked_list.py
@@ -53,26 +53,3 @@ def imprimir_lista(root):
 nums = entrar_numeros()
 head = iniciar_lista(nums)
 imprimir_lista(head)
-
-def partir(nums, izq, der):
-	i = (izq-1)
-	pivote = nums[der]
-	for j in range(izq, der):
-		if nums[j] <= pivote:
-			i = i + 1
-			nums[i], nums[j] = nums[j], nums[i]
-
-	nums[i+1], nums[der] = nums[der], nums[i+1]
-	return (i+1)
-
-def quick_sort(nums, izq, der):
-	if len(nums) == 1:
-		return nums
-	if izq < der:
-		mid = partir(nums, izq, der)
-		quick_sort(nums, izq, mid-1)
-		quick_sort(nums, mid+1, der)
-
-nums = [9, 8, 7, 2, 3, 5, 6, 4]
-quick_sort(nums, 0, len(nums)-1)
-print(nums)
