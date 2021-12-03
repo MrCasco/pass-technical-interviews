ed45f2a8741f7eb84e47321288000ad50ef9f4e3
Ernesto Casco <ernesto.casco.velazquez@hotmail.com>
2021-12-03 16:24:12 -0600

    Day 3 part two solved

--- solutions/day_3/binary_diagnostic.py
+++ solutions/day_3/binary_diagnostic.py
@@ -14,5 +14,36 @@ def binary_diagnostic(arr):
     gamma = ''.join(['1' if x > len(arr)/2 else '0' for x in res])
     return int(epsilon, 2) * int(gamma, 2)
 
+def helper(arr, type):
+    prospects = arr[:]
+    while len(prospects) > 1:
+        for i in range(len(arr[0])):
+            if len(prospects) == 1:
+                break
+            ones = []
+            zeros = []
+            majority = 0
+            for number in prospects:
+                digit = int(number[i])
+                majority += digit
+                if digit == 1:
+                    ones.append(number)
+                else:
+                    zeros.append(number)
+            if type == 'upper':
+                if majority < len(prospects)/2:
+                    prospects = zeros[:]
+                elif majority >= len(prospects)/2 and len(prospects)/2 != 0:
+                    prospects = ones[:]
+            else:
+                if majority < len(prospects)/2:
+                    prospects = ones[:]
+                else:
+                    prospects = zeros[:]
+    return int(prospects[0], 2)
+
+def binary_diagnostic_follow_up(arr):
+    return helper(arr, 'lower') * helper(arr, 'upper')
+
 arr = text_reader('day_3/binary_diagnostic.txt')
-print(binary_diagnostic(arr))
+print(binary_diagnostic_follow_up(arr))
