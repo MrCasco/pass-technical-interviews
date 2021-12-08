c18ee70b2a01b8ad1c9c6532d067eb78818bef5a
Ernesto Casco <ernesto.casco.velazquez@hotmail.com>
2021-12-08 13:40:48 -0600

    Day 4 part 2 Done

--- solutions/day_4/bingo_winner.py
+++ solutions/day_4/bingo_winner.py
@@ -1,14 +1,9 @@
 import sys
 sys.path.insert(1, 'C:\Python\check-if-want-to-pass-technical-interviews')
 
-
-def text_reader(file):
-    f = open("C:\Python\check-if-want-to-pass-technical-interviews\inputs/"+file, "r")
-    return [int(num) for num in f.readline().strip('\n').split(',')]
-
 def board_reader():
     f = open("C:\Python\check-if-want-to-pass-technical-interviews\inputs/day_4/bingo.txt", "r")
-    f.readline()
+    nums = [int(num) for num in f.readline().strip('\n').split(',')]
     f.readline()
     boards = []
     cur_board = {'rows': [0 for _ in range(5)], 'cols': [0 for _ in range(5)]}
@@ -24,7 +19,7 @@ def board_reader():
             num = int(line[col])
             cur_board[num] = (row, col)
         row += 1
-    return boards
+    return nums, boards
 
 def sum_not_checked(board):
     board.pop('rows')
@@ -32,12 +27,25 @@ def sum_not_checked(board):
     return sum(board.keys())
 
 def get_winner_board():
-    bingo_nums = text_reader('day_4/bingo.txt')
-    boards = board_reader()
+    bingo_nums, boards = board_reader()
+    for num in bingo_nums:
+        for board in boards:
+            if num in board:
+                row, col = board[num]
+                board['rows'][row] += 1
+                board['cols'][col] += 1
+                board.pop(num)
+                if board['rows'][row] == 5 or board['cols'][col] == 5:
+                    return board, num
+
+def get_last_winner_board():
+    bingo_nums, boards = board_reader()
     last_board = {}
     last_num = 0
+    winners = set()
     for num in bingo_nums:
         for i, board in enumerate(boards):
+            if i in winners: continue
             if num in board:
                 row, col = board[num]
                 board['rows'][row] += 1
@@ -46,8 +54,14 @@ def get_winner_board():
                 if board['rows'][row] == 5 or board['cols'][col] == 5:
                     last_board = board
                     last_num = num
-                    boards.pop(i)
+                    winners.add(i)
     return last_board, last_num
 
+
+# PART 1
 board, last_num = get_winner_board()
+print(sum_not_checked(board) * last_num)
+
+# PART 2
+board, last_num = get_last_winner_board()
 print(sum_not_checked(board) * last_num)
